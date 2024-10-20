f739e1ec5f7f6fededb7f01aad81f23e388ad599
Umair29-dot <umair.nazim0@gmail.com>
2024-10-20 19:26:45 +0300

    Implemented username, email,  password textfield and button

---
+++ app/src/main/java/com/umair/chatme/auth/SignInScreen.kt
@@ -0,0 +1,102 @@
+package com.umair.chatme.auth
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SignInScreen() {
+	var userName = remember {
+		mutableStateOf("")
+	}
+	var email = remember {
+		mutableStateOf("")
+	}
+	var password = remember {
+		mutableStateOf("")
+	}
+
+	Surface(
+		modifier = Modifier
+			.fillMaxSize()
+	) {
+		Column(
+			verticalArrangement = Arrangement.Center,
+			horizontalAlignment = Alignment.CenterHorizontally
+		) {
+			OutlinedTextField(
+				value = userName.value,
+				onValueChange = { userName.value = it},
+				label = { Text("Username") }
+			)
+
+			Spacer(
+				modifier = Modifier
+					.height(10.dp)
+			)
+
+			OutlinedTextField(
+				value = email.value,
+				onValueChange = { email.value = it},
+				label = { Text("Email") }
+			)
+
+			Spacer(
+				modifier = Modifier
+					.height(10.dp)
+			)
+
+			OutlinedTextField(
+				value = password.value,
+				onValueChange = { password.value = it},
+				label = { Text("Password") }
+			)
+
+			Spacer(
+				modifier = Modifier
+					.height(10.dp)
+			)
+
+			Text("Forget Password")
+
+			Spacer(
+				modifier = Modifier
+					.height(20.dp)
+			)
+
+			Button(
+				onClick = {
+
+				}
+			) {
+				Text("Login")
+			}
+
+			Spacer(
+				modifier = Modifier
+					.height(10.dp)
+			)
+
+			Text("Don't have an account? Signup")
+		}//: Column
+	}//: Surface
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+	SignInScreen()
+}
