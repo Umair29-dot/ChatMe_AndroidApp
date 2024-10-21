8844b48d39af2db8edbbbbdf84e87a1c4903a375
Umair29-dot <umair.nazim0@gmail.com>
2024-10-21 20:15:30 +0300

    created ChatScreen

---
+++ app/src/main/java/com/umair/chatme/main/chat/ChatScreen.kt
@@ -0,0 +1,15 @@
+package com.umair.chatme.main.chat
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ChatScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+	ChatScreen()
+}
